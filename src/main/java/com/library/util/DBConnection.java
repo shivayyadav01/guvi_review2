36c18c6e346c7de51aadeea1b6a92a0531177904
shivayyadav01 <shivay.24scse1180100@galgotiasuniversity.ac.in>
2025-12-30 16:11:10 +0530

    Update DBConnection.java

--- src/main/java/com/library/util/DBConnection.java
+++ src/main/java/com/library/util/DBConnection.java
@@ -4,20 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    // Apne MySQL ka sahi details yahan bharein
-    private static final String URL = "jdbc:mysql://localhost:3306/library_db"; // database naam check karein
-    private static final String USER = "root"; // apna username
-    private static final String PASS = "Shivay32006"; // apna password
+   
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";  
+    private static final String USER = "root";  
+    private static final String PASS = "Shivay32006";  
 
     public static Connection getConnection() {
         try {
-            // Driver load karna Tomcat 10+ ke liye zaroori hai
+             
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (Exception e) {
             System.out.println("Database Connection Failed: " + e.getMessage());
             e.printStackTrace();
-            return null; // Isiliye aapko null error aa raha hai
+            return null;  
         }
     }
 }
